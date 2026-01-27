0e19f231a2221726a72c9af77499c2979a4229bd
alex <pisonalex@gmail.com>
2026-01-27 21:30:17 +0100

    Feat: using time instead of datetime for cur time
    datetime gives more information than needed
    
    Issue #5

--- myGestureRecognizer/fps.py
+++ myGestureRecognizer/fps.py
@@ -1,19 +1,24 @@
-import datetime
+import time
+
 
 class FPS:
     def __init__(self):
         self._start = None
+        self._prev: float = 0
         self._numFrames = 0
 
     def start(self):
-        self._start = datetime.datetime.now()
+        self._start = time.time()
         return self
 
     def update(self):
         self._numFrames += 1
 
-    def elapsed(self):
-        return (datetime.datetime.now() - self._start).total_seconds()
+    def elapsed_since_last_frame(self):
+        return time.time() - self._prev
 
-    def fps(self):
-        return self._numFrames / self.elapsed()
+    def elapsed_seconds(self):
+        return (time.time() - self._start).total_seconds()
+
+    def get_current_fps(self):
+        return self._numFrames / self.elapsed_seconds()
