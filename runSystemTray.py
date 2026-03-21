af5983e98dc9cb3a53b1b5f69f3f531285355d4b
alex <pisonalex@gmail.com>
2026-03-21 22:01:54 +0000

    feat: nuitka commands
    Issue #10

--- runSystemTray.py
+++ runSystemTray.py
@@ -1,3 +1,19 @@
+# nuitka-project: --mode=standalone
+
+# for pyside6
+# nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --include-qt-plugins=qml
+
+# --------------- FOR OPENVINO ----------------
+# nuitka-project: --include-package=openvino_genai
+# nuitka-project: --include-package-data=openvino_genai
+
+# nuitka-project: --include-package=openvino_tokenizers
+# nuitka-project: --include-package-data=openvino_tokenizers
+
+# recognizer file for mediapipe
+# nuitka-project: --include-data-files={MAIN_DIRECTORY}/src/systemTrayDesktopApp/icon.ico=src/systemTrayDesktopApp/icon.ico
+
 from src.systemTrayDesktopApp.systemTrayApp import main
 
 
