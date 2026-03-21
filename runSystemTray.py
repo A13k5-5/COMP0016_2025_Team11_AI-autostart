22f9b3025582d00a34d2299496ff4dc8f05fe8c8
alex <pisonalex@gmail.com>
2026-03-21 23:49:08 +0000

    fix: gesture recognizer.task file included in the nuitka commands
    Issue #10

--- runSystemTray.py
+++ runSystemTray.py
@@ -9,6 +9,7 @@
 # nuitka-project: --include-package-data=openvino
 
 # recognizer file for mediapipe
+# nuitka-project: --include-data-files={MAIN_DIRECTORY}/src/video_recogniser/gesture_recogniser/gesture_recognizer.task=src/video_recogniser/gesture_recogniser/gesture_recognizer.task
 # nuitka-project: --include-data-files={MAIN_DIRECTORY}/src/systemTrayDesktopApp/icon.ico=src/systemTrayDesktopApp/icon.ico
 
 from src.systemTrayDesktopApp.systemTrayApp import main
