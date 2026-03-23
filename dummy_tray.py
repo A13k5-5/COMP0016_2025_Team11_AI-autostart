bf0123c9813fdaa743ef636a3db6687c858e6b0d
alex <pisonalex@gmail.com>
2026-03-23 03:13:07 +0000

    test: testing with dummy_tray to make sure compiling works
    Issue #46

---
+++ dummy_tray.py
@@ -0,0 +1,35 @@
+# nuitka-project: --mode=standalone
+
+# for pyside6
+# nuitka-project: --enable-plugin=pyside6
+
+from PySide6.QtGui import QIcon, QAction
+from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
+
+if __name__ == "__main__":
+    app = QApplication([])
+    app.setQuitOnLastWindowClosed(False)
+
+    # Create the icon
+    icon = QIcon("icon.png")
+
+    # Create the tray
+    tray = QSystemTrayIcon()
+    tray.setIcon(icon)
+    tray.setVisible(True)
+
+    # Create the menu
+    menu = QMenu()
+    action = QAction("A menu item")
+    action.triggered.connect(lambda: print("Menu item clicked"))
+    menu.addAction(action)
+
+    # Add a Quit option to the menu.
+    quit = QAction("Quit")
+    quit.triggered.connect(app.quit)
+    menu.addAction(quit)
+
+    # Add the menu to the tray
+    tray.setContextMenu(menu)
+
+    app.exec()
