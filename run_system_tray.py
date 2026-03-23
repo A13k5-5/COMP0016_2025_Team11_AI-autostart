1d07956895e58e090344c242af1832166973f800
alex <pisonalex@gmail.com>
2026-03-23 03:16:14 +0000

    feat: disabled person recognition
    Issue #46

--- run_system_tray.py
+++ run_system_tray.py
@@ -3,11 +3,10 @@
 
 # for pyside6
 # nuitka-project: --enable-plugin=pyside6
-# nuitka-project: --include-qt-plugins=qml
 
 # --------------- FOR OPENVINO ----------------
-# nuitka-project: --include-package=openvino
-# nuitka-project: --include-package-data=openvino
+## nuitka-project: --include-package=openvino
+## nuitka-project: --include-package-data=openvino
 
 # recognizer file for mediapipe
 # nuitka-project: --include-data-files={MAIN_DIRECTORY}/src/video_recogniser/gesture_recogniser/gesture_recognizer.task=src/video_recogniser/gesture_recogniser/gesture_recognizer.task
